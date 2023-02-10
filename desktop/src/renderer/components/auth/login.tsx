46a95e57551536a9896f413a8d913175241a37fc
Remy de Jong <rimai4@hotmail.com>
2023-02-10 14:49:36 +0100

    fix codecapi name

--- desktop/src/renderer/components/auth/login.tsx
+++ desktop/src/renderer/components/auth/login.tsx
@@ -8,7 +8,7 @@ export default function Login() {
     <div className="columns is-centered">
       <div className="column">
         <p className="mb-4">
-          Dit is de portal van [codecapi]. Klik op de knop hieronder om in te
+          Dit is de portal van [code]capi. Klik op de knop hieronder om in te
           loggen met je Google account.
         </p>
         <button className="button" onClick={() => signIn()}>
