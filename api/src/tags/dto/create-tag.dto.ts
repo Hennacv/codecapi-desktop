366c93bc055cf8f754372f36daad48a8aa971062
wessel <wessel@codecapi.nl>
2023-03-13 16:11:55 +0100

    add color field to tags

--- api/src/tags/dto/create-tag.dto.ts
+++ api/src/tags/dto/create-tag.dto.ts
@@ -1,6 +1,12 @@
-import { IsNotEmpty } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsHexColor, IsOptional } from 'class-validator';
 
 export class CreateTagDto {
   @IsNotEmpty()
   title: string;
+
+  @IsOptional()
+  @IsHexColor()
+  @Transform(({ value }) => value.toLowerCase())
+  color: string;
 }
