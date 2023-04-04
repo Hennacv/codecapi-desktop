8d2deb357de70aed70f8c6656932744e738dc08d
Henna06 <henna@codecapi.nl>
2023-04-04 11:54:37 +0200

    New question button

--- desktop/src/renderer/components/questions/question-list/question-list.tsx
+++ desktop/src/renderer/components/questions/question-list/question-list.tsx
@@ -1,12 +1,20 @@
 import { useGetQuestions } from 'renderer/hooks/use-get-questions';
 import { Question } from 'renderer/utils/types';
+import { Link } from 'react-router-dom';
+import { ButtonVariants } from 'renderer/components/ui/button/button-styles.css';
 import QuestionCard from '../question-card/question-card';
+import { NewQuestionButtonPosition } from '../new-question/new-question-styles.css';
 
-const QuestionList = () => {
+function QuestionList() {
   const { data: questions = [] } = useGetQuestions();
 
   return (
     <div>
+      <div className={NewQuestionButtonPosition}>
+        <Link className={ButtonVariants.smallSquare} to="/questions/new">
+          + New Question
+        </Link>
+      </div>
       {questions.map((question: Question) => (
         <QuestionCard key={question.id} question={question} />
       ))}
--- desktop/src/renderer/components/ui/button/button-styles.css.ts
+++ desktop/src/renderer/components/ui/button/button-styles.css.ts
@@ -1,22 +1,24 @@
-import { style, styleVariants } from "@vanilla-extract/css";
-import { vars } from "renderer/vars.css";
+import { style, styleVariants } from '@vanilla-extract/css';
+import { vars } from 'renderer/vars.css';
 
 const ButtonBase = style({
+  textDecoration: 'none',
   width: vars.width.fit,
   backgroundColor: vars.color['dark-300'],
-  border: '2px solid ' + vars.color['dark-400'],
+  color: vars.color['text-white-200'],
+  border: `2px solid ${vars.color['dark-400']}`,
   borderRadius: vars.borderRadius.medium,
   cursor: 'pointer',
   transition: vars.transition.defaultHover,
   ':hover': {
-		backgroundColor: vars.color['dark-400'],
-		transition: vars.transition.defaultHover,
+    backgroundColor: vars.color['dark-400'],
+    transition: vars.transition.defaultHover,
   },
 });
 
 const ButtonSmall = style({
   padding: vars.spacing['0.5x'] + vars.spacing['1x'],
-	fontSize: vars.fontSize.small,
+  fontSize: vars.fontSize.small,
 });
 
 const ButtonDefault = style({
@@ -26,11 +28,11 @@ const ButtonDefault = style({
 
 const ButtonDisabled = style({
   ':disabled': {
-		backgroundColor: vars.color['dark-200'],
-		color: vars.color['text-white-300'],
-		border: '2px solid ' + vars.color['dark-300'],
-		cursor: 'not-allowed'
-  }
+    backgroundColor: vars.color['dark-200'],
+    color: vars.color['text-white-300'],
+    border: `2px solid ${vars.color['dark-300']}`,
+    cursor: 'not-allowed',
+  },
 });
 
 const ButtonSmallSquare = style({
@@ -42,7 +44,7 @@ const ButtonDefaultSquare = style({
 });
 
 export const ButtonVariants = styleVariants({
-	small: [ButtonBase, ButtonSmall],
+  small: [ButtonBase, ButtonSmall],
   smallSquare: [ButtonBase, ButtonSmall, ButtonSmallSquare],
   default: [ButtonBase, ButtonDefault],
   defaultSquare: [ButtonBase, ButtonDefault, ButtonDefaultSquare],
