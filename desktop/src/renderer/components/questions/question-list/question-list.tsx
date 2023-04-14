6e7ccaac5f3f910d8ec291e5effa0efa89a1d269
Wouter Ronteltap <wouter.ronteltap@sanoma.com>
2023-04-14 14:42:09 +0200

    Fix derived state

--- desktop/src/renderer/components/questions/question-list/question-list.tsx
+++ desktop/src/renderer/components/questions/question-list/question-list.tsx
@@ -1,43 +1,35 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Question, Tag } from "renderer/utils/types";
-import { useGetQuestions } from "renderer/hooks/use-get-questions";
-import { SFContainer } from "renderer/components/ui/search/search-styles.css";
-import { NewQuestionButtonPosition } from "../new-question/new-question-styles.css";
-import QuestionCard from "../question-card/question-card";
-import Button from "renderer/components/ui/button/button";
-import Filter from "renderer/components/ui/filter/filter";
-import Search from "renderer/components/ui/search/search";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Question, Tag } from 'renderer/utils/types';
+import { useGetQuestions } from 'renderer/hooks/use-get-questions';
+import { SFContainer } from 'renderer/components/ui/search/search-styles.css';
+import { NewQuestionButtonPosition } from '../new-question/new-question-styles.css';
+import QuestionCard from '../question-card/question-card';
+import Button from 'renderer/components/ui/button/button';
+import Filter from 'renderer/components/ui/filter/filter';
+import Search from 'renderer/components/ui/search/search';
 
 const QuestionList = () => {
   const navigate = useNavigate();
   const { data = [] } = useGetQuestions();
-  const [questions,setQuestions]=useState(data);
-  const [searchTerm,setSearchTerm]=useState('');
+  const [searchTerm, setSearchTerm] = useState('');
   const [tags, setTags] = useState<Tag[]>([]);
 
-  useEffect(()=>{
-    setQuestions(data)
-  },[data])
+  let result = data.filter((question) =>
+    question.title.toLowerCase().includes(searchTerm)
+  );
 
-  useEffect(()=> {
-    let result = data.filter((question) => question.title.toLowerCase().includes(searchTerm));
-
-    if(!!tags.length){
-      result = result.filter((question) => {
-        return question.tags.some((questionTag)=>{
-          return tags.some((tag)=>tag.id === questionTag.id)
-        })
-      })
-    }
-
-    setQuestions(result)
-  },[tags,searchTerm])
-
+  if (!!tags.length) {
+    result = result.filter((question) => {
+      return question.tags.some((questionTag) => {
+        return tags.some((tag) => tag.id === questionTag.id);
+      });
+    });
+  }
 
   const onNewQuestion = () => {
-    navigate("/questions/new");
-  }
+    navigate('/questions/new');
+  };
 
   return (
     <div>
@@ -53,11 +45,12 @@ const QuestionList = () => {
           onClick={() => onNewQuestion()}
         />
       </div>
-      {questions.map((question: Question) => (
+
+      {result.map((question: Question) => (
         <QuestionCard key={question.id} question={question} />
       ))}
     </div>
   );
-}
+};
 
 export default QuestionList;
