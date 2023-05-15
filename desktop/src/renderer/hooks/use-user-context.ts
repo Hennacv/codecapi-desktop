604591ca6e37e44fcb13284f2f6143769c002499
wessel <wessel@codecapi.nl>
2023-05-15 12:51:10 +0200

    add use-user-context-hook

---
+++ desktop/src/renderer/hooks/use-user-context.ts
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { AuthContext } from "renderer/root";
+
+export const useUserContext = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error(
+      'useUserContext must be used within the AuthProvider'
+    );
+  }
+  return context;
+};
