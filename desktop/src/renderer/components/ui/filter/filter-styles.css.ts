a6836f5a8105c417b0e4560efcc480cdba5e65da
Henna06 <henna@codecapi.nl>
2023-05-22 12:01:57 +0200

    Refactor modal as separate component

--- desktop/src/renderer/components/ui/filter/filter-styles.css.ts
+++ desktop/src/renderer/components/ui/filter/filter-styles.css.ts
@@ -1,44 +1,13 @@
 import { style, styleVariants } from "@vanilla-extract/css";
 import { vars } from "renderer/vars.css";
 
-const Modal = style({
-  backgroundColor: vars.color['dark-opaque'],
-  position: 'absolute',
-  inset: 0,
-  opacity: 0,
-  transition: `all 0.3s ease-in-out`,
-  pointerEvents: 'none',
-  zIndex: 5,
-})
-
-const Show = style({
-  opacity: 1,
-  pointerEvents: 'visible',
-})
-
-export const ModalStyles = styleVariants({
-  basic: [ Modal ],
-  show: [ Modal, Show ],
-
-})
-
-export const ModalContent = style({
-  backgroundColor: vars.color['dark-200'],
-  position: 'absolute',
-  borderRadius: vars.borderRadius.large,
-  padding: vars.spacing['1.25x'],
-  width: '90%',
-  right: vars.spacing['2x'],
-  top: vars.spacing['4.75x'],
-})
-
-export const ModalTitle = style({
+export const FilterTitle = style({
   textTransform: 'uppercase',
   fontSize: vars.fontSize.extraSmall,
   color: vars.color['text-white-200'],
 })
 
-export const ModalTag = style({
+export const FilterTag = style({
   margin: `${vars.spacing['0.75x']} 0`,
   display: 'flex',
   flexDirection: 'row',
