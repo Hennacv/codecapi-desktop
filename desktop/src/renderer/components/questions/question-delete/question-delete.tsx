246463500ebcdd2a1a3a25fd5812d1863cc1cbbc
wessel <wessel@codecapi.nl>
2023-05-25 12:57:29 +0200

    fix redirect after deleting question

--- desktop/src/renderer/components/questions/question-delete/question-delete.tsx
+++ desktop/src/renderer/components/questions/question-delete/question-delete.tsx
@@ -1,10 +1,8 @@
-import { useDeletePost } from 'renderer/hooks/use-delete-question';
-import Modal from 'renderer/components/ui/modal/modal';
+import { useDeleteQuestion } from 'renderer/hooks/use-delete-question';
+import Modal from "renderer/components/ui/modal/modal";
 import Button from 'renderer/components/ui/button/button';
-import {
-  DeleteButtonContainer,
-  DeleteQuestionMessage,
-} from './question-delete-styles.css';
+import { DeleteButtonContainer, DeleteQuestionMessage } from './question-delete-styles.css';
+import { useNavigate } from 'react-router-dom';
 
 interface DeleteProps {
   id: string | undefined;
@@ -12,11 +10,12 @@ interface DeleteProps {
   onClose: (arg0: boolean) => void;
 }
 
-const QuestionDelete = ({ id, isShown, onClose }: DeleteProps) => {
-  const { mutate } = useDeletePost();
-  const deleteQuestion = (id: string | undefined) => {
-    mutate(id);
-  };
+const QuestionDelete = ({id, isShown, onClose}: DeleteProps) => {
+  const navigate = useNavigate();
+
+  const deleteQuestion = useDeleteQuestion({
+    onSuccess: () => navigate("/questions"),
+  });
 
   return (
     <Modal isShown={isShown} onClose={() => onClose(false)}>
@@ -24,19 +23,19 @@ const QuestionDelete = ({ id, isShown, onClose }: DeleteProps) => {
         <p> Are you sure you want to delete this question?</p>
       </div>
       <div className={DeleteButtonContainer}>
-        <Button
-          text="Cancel"
-          variant="small"
-          type="button"
-          onClick={() => onClose(false)}
-        />
-        <Button
-          text="Delete"
-          variant="delete"
-          type="button"
-          onClick={() => deleteQuestion(id)}
-        />
-      </div>
+      <Button
+       text="Cancel"
+       variant="small"
+       type="button"
+       onClick={() => onClose(false)}
+       />
+      <Button
+       text="Delete"
+       variant="delete"
+       type="button"
+       onClick={() => deleteQuestion.mutate(id)}
+       />
+       </div>
     </Modal>
   );
 };
