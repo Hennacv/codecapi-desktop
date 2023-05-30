7788d248e0ab6dca925361f91aa5b5bc0f94f9b4
Henna06 <henna@codecapi.nl>
2023-05-30 12:10:11 +0200

    Filter on skills with filter term display

--- desktop/src/renderer/components/ui/filter/filter-styles.css.ts
+++ desktop/src/renderer/components/ui/filter/filter-styles.css.ts
@@ -1,6 +1,15 @@
 import { style, styleVariants } from '@vanilla-extract/css';
 import { vars } from 'renderer/vars.css';
 
+export const FilterTermContainer = style({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  flexWrap: 'wrap',
+  gap: vars.spacing['0.5x'],
+  marginBottom: vars.spacing['1.25x'],
+})
+
 export const FilterTitle = style({
   textTransform: 'uppercase',
   fontSize: vars.fontSize.extraSmall,
--- desktop/src/renderer/components/users/user-list.tsx
+++ desktop/src/renderer/components/users/user-list.tsx
@@ -1,25 +1,63 @@
 import { useGetUsers } from 'renderer/hooks/use-get-users';
-import { User } from 'renderer/utils/types';
+import { Tag, User } from 'renderer/utils/types';
 import { UserCardsContainer } from './user-card/user-card-styles.css';
 import UserCard from './user-card/user-card';
 import { SFContainer } from '../ui/search/search-styles.css';
 import { useState } from 'react';
 import Search from '../ui/search/search';
+import Button from '../ui/button/button';
+import Filter from '../ui/filter/filter';
+import TagCard from '../tags/tag-card/tag-card';
+import { FilterTermContainer } from '../ui/filter/filter-styles.css';
 
 const UserList = () => {
   const { data: users = [] } = useGetUsers();
   const [searchTerm, setSearchTerm] = useState('');
-
-  console.log(users)
+  const [tags, setTags] = useState<Tag[]>([]);
+  const [isShown, setIsShown] = useState(false);
 
   let result = users.filter((user) =>
     user.name.toLowerCase().includes(searchTerm)
   );
 
+  if (!!tags.length) {
+    result = result.filter((user) => {
+      return user.tags.some((userTag) => {
+        return tags.some((tag) => tag.id === userTag.id);
+      });
+    });
+  }
+
   return (
     <>
       <div className={SFContainer}>
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <Button
+          text="Filter"
+          variant="small"
+          type="button"
+          onClick={() => setIsShown(true)}
+        />
+        <Filter
+          tags={tags}
+          setTags={setTags}
+          isShown={isShown}
+          onClose={() => setIsShown(false)}
+        />
+      </div>
+      <div className={FilterTermContainer}>
+      {!!tags.length && (
+            <>
+              {tags.map((tag) => (
+                <TagCard
+                  key={tag.id}
+                  title={tag.title}
+                  color={tag.color}
+                  variant="small"
+                />
+              ))}
+            </>
+          )}
       </div>
       <div className={UserCardsContainer}>
         {result
