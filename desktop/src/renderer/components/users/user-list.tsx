3998c08fe1baca86c51f8b6cf91b74a49b4e9b56
Henna06 <henna@codecapi.nl>
2023-05-30 11:19:20 +0200

    Search added

--- desktop/src/renderer/components/users/user-list.tsx
+++ desktop/src/renderer/components/users/user-list.tsx
@@ -1,20 +1,34 @@
 import { useGetUsers } from 'renderer/hooks/use-get-users';
 import { User } from 'renderer/utils/types';
 import { UserCardsContainer } from './user-card/user-card-styles.css';
-
 import UserCard from './user-card/user-card';
+import { SFContainer } from '../ui/search/search-styles.css';
+import { useState } from 'react';
+import Search from '../ui/search/search';
 
 const UserList = () => {
   const { data: users = [] } = useGetUsers();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  console.log(users)
+
+  let result = users.filter((user) =>
+    user.name.toLowerCase().includes(searchTerm)
+  );
 
   return (
-    <div className={UserCardsContainer}>
-      {users
-        .sort((a, b) => a.id - b.id)
-        .map((user: User, index) => (
-          <UserCard user={user} key={index} />
-        ))}
-    </div>
+    <>
+      <div className={SFContainer}>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+      <div className={UserCardsContainer}>
+        {result
+          .sort((a, b) => a.id - b.id)
+          .map((user: User, index) => (
+            <UserCard user={user} key={index} />
+            ))}
+      </div>
+    </>
   );
 };
 
